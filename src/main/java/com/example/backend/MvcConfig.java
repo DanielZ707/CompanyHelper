4b7469342d636b6e41041c3e467b4df9c50bf3a8
Daniel <daniel.z.7000000@gmail.com>
2023-06-05 10:46:57 +0200

    added necessary dependencies and refactored configuration

--- src/main/java/com/example/backend/MvcConfig.java
+++ src/main/java/com/example/backend/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("**/**");
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
